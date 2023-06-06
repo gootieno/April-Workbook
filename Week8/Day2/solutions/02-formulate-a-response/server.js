93adb15bf3f77b82370ca366558eb540bb669e7e
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-06-06 10:38:43 -0700

    http formulate response solution

--- Week8/Day2/solutions/02-formulate-a-response/server.js
+++ Week8/Day2/solutions/02-formulate-a-response/server.js
@@ -1,18 +1,49 @@
 // Your code here
 
 // require the http package
+const http = require("http");
+// create a server
+const server = http.createServer((req, res) => {
+  //get response body
+  const responseBody = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World!</title>
+  </head>
+  <body>
+    <h1>Hello there!</h1>
+  </body>
+  </html>
+`;
+  //set status code
+  res.statusCode = 200;
+  //set header
+  res.setHeader("Content-Type", "text/html");
+  //return response body
+  return res.end(responseBody);
+  
 
-// create a server 
+//   if(req.method === 'GET' && req.url === '/cats'){
+//     // server code stuff
+//     const responseBody = 'Dog Club'
 
-    // use response body from readme
+//     return res.end('Dog Club');
+//   }
 
-    // set status code
+//   if(req.method === 'GET' && req.url === '/dogs'){
+//     // server code stuff
 
-    // set content  type
+//     return res.end(responseBody);
+//   }
 
-    // return response body (res.end() or res.write())
-    // return res.end(responseBody)
+  
+
+});
 
 // create a port
-
+const port = 5000;
 // listen on the port
+server.listen(port, () => console.log(`listening on port ${port}`));
