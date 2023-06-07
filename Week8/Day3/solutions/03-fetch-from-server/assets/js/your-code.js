74987decd498a3b09c84303e085d04909b9850b9
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-06-07 14:12:08 -0700

    fetch solutions

--- Week8/Day3/solutions/03-fetch-from-server/assets/js/your-code.js
+++ Week8/Day3/solutions/03-fetch-from-server/assets/js/your-code.js
@@ -1,19 +1,47 @@
 export function getAllDogs() {
-    // Your code here
+  // Your code here
+  const url = "/dogs";
+  return fetch(url);
 }
 
 export function getDogNumberTwo() {
-    // Your code here
+  // Your code here
+  const url = "/dogs/2";
+  return fetch(url);
 }
 
 export function postNewDog() {
-    // Your code here
+  // Your code here
+
+  const url = "/dogs";
+  const headers = { "Content-Type": "application/x-www-form/url-encoded" };
+  const body = new URLSearchParams({
+    name: "Kaido",
+    age: 0.4,
+  });
+
+  const options = {
+    method: "POST",
+    headers,
+    body,
+  };
+
+  return fetch(url, options)
 }
 
 export function postNewDogV2(name, age) {
-     // Your code here
+  // Your code here
+  return fetch('/dogs', {
+    method: 'POST',
+    headers:{ "Content-Type": "application/x-www-form/url-encoded" },
+    body: new URLSearchParams({name, age})
+  })
 }
 
 export function deleteDog(id) {
-      // Your code here
+  // Your code here
+  return fetch(`/dogs/${id}/delete`, {
+    method: "POST",
+    headers: {AUTH: "ckyut5wau0000jyv5bsrud90y"},
+  });
 }
--- Week8/Day3/solutions/04-fetch/fetch-requests.js
+++ Week8/Day3/solutions/04-fetch/fetch-requests.js
@@ -7,20 +7,43 @@ Paste your code for fetch requests here once you finish each task.
 
 // Your code here
 
-
-
 /* ====== 2. Print true if the status of the response was successful ====== */
 
 // Your code here
-
-
 
 /* =================== 3. Print the Content-Type Header =================== */
 
 // Your code here
 
-
-
 /* ============== 4. Print the body of the response as text =============== */
 
 // Your code here
+
+const getProducts = async () => {
+  const response = await fetch("/products");
+
+  console.log("response ", response);
+  console.log("response status ", response.status);
+  console.log("response ok? ", response.ok);
+  console.log("response headers ", response.headers.get("Content-Type"));
+
+  const responseBody = await response.text();
+  console.log("response body ", responseBody);
+};
+
+getProducts();
+
+// const getProducts = () => {
+//   fetch("/products")
+//     .then((response) => {
+//       console.log("response ", response);
+//       console.log("response status ", response.status);
+//       console.log("response ok? ", response.ok);
+//       console.log("response headers ", response.headers.get("Content-Type"));
+//       return response.text();
+//     })
+//     .then((responseData) => console.log("response data ", responseData));
+// };
+
+
+// getProducts()
