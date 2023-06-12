c2277177d967523953757226a2d0aeab488060b8
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-06-12 16:21:19 -0700

    es6 import solution

---
+++ Week9/Day1/solutions/02-es6-modules/assets/js/nav.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    // creates the navbar container and adds default styles
+
+}
+
+export const changeNavLinks = () => {
+    // depending on page will display different nav links
+    // ie:logged in vs not logged in
+}
+
+export default Navbar
