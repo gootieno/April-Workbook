0fca86cc58540d29f453498aaa93ebe7151f5682
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-06-14 08:19:24 -0700

    add/remove event listener polya

---
+++ Week9/Day3/solutions/01-add-remove-event-listeners/events.js
@@ -0,0 +1,12 @@
+// Your code here
+
+/*
+When the #red-input input contains the word "red", 
+change the background color of the input to red. 
+Otherwise, remain transparent.
+
+
+// get element with #red-input
+// add event listener to #red-input element and listen for an event
+// write conditional to check if input value is red or not red
+*/
